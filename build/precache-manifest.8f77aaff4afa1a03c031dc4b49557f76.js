1ec2cd6f0f2d2d3b8c34b43577ca776563dbcc0a
verydecent <wnjhwng@gmail.com>
2019-06-10 00:26:03 -0700

    yarn build

---
+++ build/precache-manifest.8f77aaff4afa1a03c031dc4b49557f76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6179944680ed939cd626532d29fabe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23e328faa4bc3928a7b7",
+    "url": "/static/css/main.c99d133d.chunk.css"
+  },
+  {
+    "revision": "c0d2c0576893ebf4b1e7",
+    "url": "/static/js/2.c53dee76.chunk.js"
+  },
+  {
+    "revision": "23e328faa4bc3928a7b7",
+    "url": "/static/js/main.59a6c63f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
