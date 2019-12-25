b6ea418fede5e9cc76fc3d200f1556e405bc0a07
verydecent <wnjhwng@gmail.com>
2019-12-24 16:26:05 -0800

    Creates component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,19 +1,11 @@
 import React from 'react';
 
-const Card = () => {
+export const Card = ({ title, children, footer }) => {
     return (
-        <div className="card-container">
-            <div className="card">
-                <h1 className="card-title">Wonjae Hwang</h1>
-                <ul className="card-list">
-                    <li className="card-list-item"><a href="/">About</a></li>
-                    <li className="card-list-item"><a href="/">Portfolio</a></li>
-                    <li className="card-list-item"><a href="/">Contact</a></li>
-                </ul>
-                <div className="footer">EST. 2020</div>
-            </div>
+        <div className="card">
+            <h1 className="card-title">{title}</h1>
+            {children}
+            <div className="footer">{footer}</div>
         </div>
     );
 }
-
-export default CanvasDrawImage;
