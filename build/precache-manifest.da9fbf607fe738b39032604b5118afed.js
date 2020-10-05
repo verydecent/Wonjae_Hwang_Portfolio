04bcce1646428260fcb794aefbff0774bb73f65b
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-05 01:04:28 -0700

    npm run build

---
+++ build/precache-manifest.da9fbf607fe738b39032604b5118afed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e371d670913e4653420eebad7b2715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "889fa0f04caea54338f2",
+    "url": "/static/css/main.053fa491.chunk.css"
+  },
+  {
+    "revision": "86ea84d6424fbcae0561",
+    "url": "/static/js/2.116328af.chunk.js"
+  },
+  {
+    "revision": "bf71ac297fcfe9a3d2d199a48fc3f60c",
+    "url": "/static/js/2.116328af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "889fa0f04caea54338f2",
+    "url": "/static/js/main.2ea636cf.chunk.js"
+  },
+  {
+    "revision": "0d5c55cba73e9143b384",
+    "url": "/static/js/runtime-main.ca4794fc.js"
+  }
+]);
